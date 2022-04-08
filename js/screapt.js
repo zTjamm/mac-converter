eedf155d7a27e699d838013a3a21beb754599886
zTjamm <jamm85@bk.ru>
2022-04-08 12:50:48 +0300

    done converrt
    ok

--- js/screapt.js
+++ js/screapt.js
@@ -16,7 +16,6 @@ macIn.addEventListener("blur", (e) => {
     macCdata.textContent = lm.replace(/(....\B)/g, "$1.");
     macHp.textContent = lm.replace(/(....\B)/g, "$1-");
 })
-
 // const macTest = 'ab:Cd-47.f5:Ac-B3';
 // console.log(macTest.toLowerCase().replace(/[:.-]/g, ""));
 
